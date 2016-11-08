bcc95b21f292514f49b645d727cae95b89726ca2
Nicolas Payot <nicolas.payot@gmail.com>
2016-11-08 23:13:32 +0100

    fix(app): fix data

--- src/app/message/messages.js
+++ src/app/message/messages.js
@@ -1,7 +1,42 @@
 export const MESSAGES = [
-  { avatar: '/img/eddard.png', sender: 'eddard', timestamp: '01:00 AM', content: 'The man who passes the sentence should swing the sword.' },
-  { avatar: '/img/khaleesi.png', sender: 'khaleesi', timestamp: '02:00 AM', content: 'The next time you raise a hand to me will be the last time you have hands!' },
-  { avatar: '/img/cersei.png', sender: 'cersei', timestamp: '03:00 AM', content: 'When you play the game of thrones, you win or you die.' },
-  { avatar: '/img/tyrion.png', sender: 'tyrion', timestamp: '04:00 AM', content: 'The mad king did as he liked, has your uncle Jaime ever told you what happened to him?' },
-  { avatar: '/img/jaqenhghar.png', sender: 'jaqenhghar', timestamp: '05:00 AM', content: 'A girl gives a man his own name?' }
+  {
+    content: 'The man who passes the sentence should swing the sword.',
+    timestamp: '01:00 AM',
+    sender: {
+      nickname: 'eddard',
+      avatar: '/img/eddard.png'
+    }
+  },
+  {
+    content: 'The next time you raise a hand to me will be the last time you have hands!',
+    timestamp: '02:00 AM',
+    sender: {
+      nickname: 'khaleesi',
+      avatar: '/img/khaleesi.png'
+    }
+  },
+  {
+    content: 'When you play the game of thrones, you win or you die.',
+    timestamp: '03:00 AM',
+    sender: {
+      nickname: 'cersei',
+      avatar: '/img/cersei.png'
+    }
+  },
+  {
+    content: 'The mad king did as he liked, has your uncle Jaime ever told you what happened to him?',
+    timestamp: '04:00 AM',
+    sender: {
+      nickname: 'tyrion',
+      avatar: '/img/tyrion.png'
+    }
+  },
+  {
+    content: 'A girl gives a man his own name?',
+    timestamp: '05:00 AM',
+    sender: {
+      nickname: 'jaqenhghar',
+      avatar: '/img/jaqenhghar.png'
+    }
+  }
 ];
