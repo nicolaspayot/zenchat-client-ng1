ab0209d5e1bc67468911082891cd1b1aae998072
Nicolas Payot <nicolas.payot@gmail.com>
2016-11-09 21:04:05 +0100

    feat(app): add main component

---
+++ src/app/zenchat.component.js
@@ -0,0 +1,3 @@
+export const zenchat = {
+  template: require('./zenchat.html')
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import angular from 'angular';
+
+import {zenchat} from './app/zenchat.component';
+
+angular.module('zenchat', [])
+  .component('zenchat', zenchat);
