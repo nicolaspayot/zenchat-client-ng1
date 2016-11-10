750f7188a62e0baed26eeae191f3c924732f220c
Nicolas Payot <nicolas.payot@gmail.com>
2016-11-10 17:18:59 +0100

    fix(app): remove messages data

--- src/app/message/messages.js
+++
@@ -1,7 +0,0 @@
-export const MESSAGES = [
-  { avatar: '/img/eddard.png', sender: 'eddard', timestamp: '01:00 AM', content: 'The man who passes the sentence should swing the sword.' },
-  { avatar: '/img/khaleesi.png', sender: 'khaleesi', timestamp: '02:00 AM', content: 'The next time you raise a hand to me will be the last time you have hands!' },
-  { avatar: '/img/cersei.png', sender: 'cersei', timestamp: '03:00 AM', content: 'When you play the game of thrones, you win or you die.' },
-  { avatar: '/img/tyrion.png', sender: 'tyrion', timestamp: '04:00 AM', content: 'The mad king did as he liked, has your uncle Jaime ever told you what happened to him?' },
-  { avatar: '/img/jaqenhghar.png', sender: 'jaqenhghar', timestamp: '05:00 AM', content: 'A girl gives a man his own name?' }
-];
